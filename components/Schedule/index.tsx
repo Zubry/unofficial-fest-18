4ccfb427a0b0c0e2edcd6133d22c43032ae70f70
jamessteele <jamessteele@IT-ES-002AM.local>
2019-09-19 17:34:11 -0400

    Make it prettier

--- components/Schedule/index.tsx
+++ components/Schedule/index.tsx
@@ -1,35 +1,57 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-import { Container, Content, Text } from 'native-base'
+import { Container, Content, View, Card, CardItem, Header, Body, Left, Text, List, ListItem, Button, Icon, Right } from 'native-base'
+
 import { groupBy, toPairs } from 'ramda'
 
-import EventGroup from '../EventGroup'
-
-export default class Schedule extends Component {
-  state = {
-    schedule: []
-  }
-
-  componentDidMount() {
-    fetch('https://api.thefestfl.com/fest18/events')
-      .then((response) => response.json())
-      .then((response) => this.setState({ schedule: toPairs(groupBy(({ start_time }) => start_time, response)) }))
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
-  render() {
-    return (
-      <Container>
-        <Content>
-          {
-            this.state.schedule.map(([timeBlock, schedule]) => (
-              <EventGroup timeBlock={timeBlock} schedule={schedule} key={timeBlock}/>
-            ))
-          }
-        </Content>
-      </Container>
-    )
-  }
+export default function schedule({ day, schedule }) {
+  const byVenue = toPairs(groupBy(({ venue_name }) => venue_name, schedule))
+  return (
+    <Container>
+      <Header>
+        <Left>
+          <Button transparent>
+            <Icon name='arrow-back' />
+          </Button>
+        </Left>
+        <Body>
+          <Text>
+            {
+              day === 1
+                ? 'Friday'
+              : day === 2
+                ? 'Saturday'
+              : 'Sunday'
+            }
+          </Text>
+        </Body>
+        <Right />
+      </Header>
+      <Content>
+        {
+          byVenue.map(([venue, shows]) => (
+            <Card key={venue}>
+              <CardItem header>
+                <Text>{venue}</Text>
+              </CardItem>
+              <CardItem>
+                <List style={{ width: "100%" }}>
+                  {
+                    shows.map(show => (
+                      <ListItem key={show.event_id} noIndent>
+                        <Text>{show.start_string} { show.performer ? show.performer : show.memo }</Text>
+                      </ListItem>
+                    ))
+                  }
+                </List>
+              </CardItem>
+            </Card>
+          ))
+        }
+      </Content>
+    </Container>
+  )
 }
+
+
+
